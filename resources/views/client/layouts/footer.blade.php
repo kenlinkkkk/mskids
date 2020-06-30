6e14851d7ae3d67dfd21bfa331ff7451bc297a29
Thanh Tung Nguyen <tungnt15ptit@gmail.com>
2020-06-30 15:59:55 +0700

    fix

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -4,22 +4,23 @@
             <div class="col-md-10 col-lg-4">
                 <div class="footer-info mb-40">
                     <img src="{{ asset('assets/client/images/logo-sk.png') }}" width="175" alt=footer-logo>
+                    <a href="#"><img src="{{ asset('assets/client/images/dathongbao.png') }}" width="117" alt=footer-logo></a>
                 </div>
             </div>
             <div class="col-md-10 col-lg-8">
                 <div class="footer-links mb-40">
                     <h5 class=h5-sm>Thông tin công ty</h5>
                     <ul class=clearfix>
-                        <li><i class="fas fa-map-marker-alt"></i>Tòa nhà Ford Thăng Long<br />105 - đường Láng Hạ, quận Đống Đa, Hà Nội.</li>
+                        <li><i class="fas fa-map-marker-alt"></i> <strong>Tòa nhà Ford Thăng Long</strong><br />105 - đường Láng Hạ, quận Đống Đa, Hà Nội.</li>
                         <li>
-                            <i class="fas fa-phone-volume"></i>
+                            <i class="fas fa-phone-volume"></i><strong> Điện thoại:</strong>
                             <a href="tel:+02435773333">024.3577 3333</a><br />
                         </li>
                         <li>
-                            <i class="fas fa-newspaper"></i>Mã số doanh nghiệp: 0102577251 do Sở Kế hoạch & Đầu tư TP Hà Nội cấp lần đầu ngày 24/12/2007 - Đăng ký thay đổi lần thứ 8, ngày 26 tháng 11 năm 2018
+                            <i class="fas fa-newspaper"></i><strong> Mã số doanh nghiệp:</strong> 0102577251 do Sở Kế hoạch & Đầu tư TP Hà Nội cấp lần đầu ngày 24/12/2007 - Đăng ký thay đổi lần thứ 8, ngày 26 tháng 11 năm 2018
                         </li>
                         <li>
-                            <i class="fas fa-newspaper"></i>Giấy phép ICP số 2959/GP-TTĐT do Sở Thông tin và Truyền thông Hà Nội cấp ngày 30/10/2014
+                            <i class="fas fa-newspaper"></i><strong> Giấy phép: </strong> ICP số 2959/GP-TTĐT do Sở Thông tin và Truyền thông Hà Nội cấp ngày 30/10/2014
                         </li>
                     </ul>
                 </div>
